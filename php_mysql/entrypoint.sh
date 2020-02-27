1f69e5a621528829aedc18ee724da065cfb463ae
denniscarrazeiro <denniscarrazeiro@gmail.com>
2020-02-27 07:36:35 -0300

    3º ajuste de desenvolvimento

--- php_mysql/entrypoint.sh
+++ php_mysql/entrypoint.sh
@@ -2,3 +2,9 @@
 
 /usr/sbin/apache2ctl start
 /usr/bin/mysqld_safe
+
+_FILE=/opt/mariadb-backup/${MYSQL_DATABASE}.sql
+if test -f "${_FILE}"; then
+	mysql -uroot -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE}"
+	mysql -uroot ${MYSQL_DATABASE} < /opt/mariadb-backup/${MYSQL_DATABASE}.sql    
+fi
